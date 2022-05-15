4fbaaeb7f32cfbde239277eed611d9fa092f20fe
Landon Buckland <landon.buckland@gmail.com>
2022-05-15 02:15:16 -0400

    state transition table compatibility
    +separate constructor which uses a transition table
    +method returning an automata's transition table
    +generated symbols are now letters instead of numbers
    -indexing errors
    -print statements / test code

--- automata.py
+++ automata.py
@@ -8,25 +8,34 @@ import os
 import my_networkx as my_nx
 from giftools import render
 
-class Automata:
-    def __init__(self, initial, accept, symbols, graph = None, state_count = 5):
+class Automaton:
+    def __init__(self, initial = 1, accept = [1], symbols = None, graph = None, state_count = 1, state_table = None):
         self.initial = initial #vector
         self.state = initial
         self.accept = accept #states where the outcome is accepted
         self.symbols = symbols #alphabet that the language is constructed from
 
-        if graph == None: #constructs a random graph if none is supplied
+        if (graph is None) and (state_table is None): #constructs a random graph on state_count many vertices if none is supplied
             graph = nx.DiGraph()
             graph.add_nodes_from([x+1 for x in range(state_count)])
             for node in graph.nodes:
                 for letter in symbols:
                     graph.add_edges_from([(node, randint(1,state_count),{'w':letter})])
             self.graph = graph
+        elif (graph is None): #constructor from state table
+            self.state_count = len(state_table)
+            self.symbols = [chr(ord('a') + x) for x in range(len(state_table[0]))]
+            graph = nx.DiGraph()
+            graph.add_nodes_from([(x+1) for x in range(self.state_count)])
+            for source in graph.nodes:
+                for letter in range(len(self.symbols)):
+                    graph.add_edges_from([(source, state_table[source-1][letter], {'w':chr(ord('a') + letter)})])
+            self.graph = graph
         else:
             self.state_count = len(graph.nodes)
-            self.graph = graph #underlying graph of the automata, nodes are unit vectors, edges already weighted with symbols
+            self.graph = graph #underlying graph of the automata, nodes integers, edges already weighted with symbols
 
-    def decide(self, word, show_trace = False):
+    def decide(self, word, show_trace = False): #returns true when the automata accepts the word after iterating
         self.state = self.initial
         count = 0
         for letter in word:
@@ -85,8 +94,8 @@ class Automata:
     def enumerate_language(self, max_length):
         language = []
 
-        for n in range(max_length+1):
-            language = language + self.enumerate_words(n)
+        for n in range(max_length):
+            language = language + self.enumerate_words(n+1)
 
         return language
 
@@ -95,13 +104,21 @@ class Automata:
         edge_weights = nx.get_edge_attributes(self.graph,'w')
 
         for letter in self.symbols:
-            system[letter] = np.zeros((self.state_count,self.state_count))
+            system[letter] = np.zeros((self.state_count,self.state_count), dtype=int)
+
         for edge in self.graph.edges:
-            elem = np.zeros((self.state_count,self.state_count))
+            elem = np.zeros((self.state_count,self.state_count), dtype=int)
             elem[edge[1]-1][edge[0]-1] = 1
             system[edge_weights[edge]] += elem
 
         return system
+
+    def state_transition_table(self):
+        edge_weights = nx.get_edge_attributes(self.graph,'w')
+        table = np.zeros((self.state_count,len(self.symbols)), dtype=int)
+        for edge in self.graph.edges:
+            table[edge[0]-1][self.symbols.index(edge_weights[edge])] = edge[1]
+        return table
 
     def plot(self, label, distinguished = [], show = False, color = 'green'):
         figure = my_nx.plot_graph(self.graph, label, distinguished, color)
@@ -109,18 +126,3 @@ class Automata:
             plt.show()
         figure.savefig("frames/" + label + ".png", bbox_inches='tight',pad_inches=0)
         plt.close()
-
-
-G = nx.DiGraph()
-G.add_edges_from([(1,2,{'w':'a'}),(1,3,{'w':'b'}),(3,1,{'w':'a'}),(2, 2,{'w':'b'}),(3,3,{'w':'b'}),(2,3,{'w':'a'})])
-
-test = Automata(1,[1],['a','b'],G)
-#print(test.decide('baabaaa', True))
-#print(test.enumerate_language(5))
-test.plot("test")
-print(test.linearize())
-
-#test = Automata(1,[1],{'a','b','c'})
-#test.plot('figure')
-#test.decide('abcabcabc',True)
-#print(test.enumerate_language(6))
